1658d102b7e3223f5dd80d5413fbfa40df6d5854
michał_śmiłowski <112591167+Malarz12@users.noreply.github.com>
2024-12-09 16:44:29 +0100

    Update lab04.sql

--- 3 sem/bd/lab04/lab04.sql
+++ 3 sem/bd/lab04/lab04.sql
@@ -1,14 +1,14 @@
 WITH co_authors AS (
     SELECT DISTINCT A.Autor AS Autor1, B.Autor AS Autor2
-    FROM Autorstwo A
-    JOIN Autorstwo B ON A.Praca = B.Praca
+    FROM Autorstwo2 A
+    JOIN Autorstwo2 B ON A.Praca = B.Praca
     WHERE A.Autor <> B.Autor
 ),
 hierarchical_query AS (
-    SELECT Autor2 AS Autor, LEVEL AS Level2
+    SELECT Autor2 AS Autor, LEVEL - 1 AS Level2
     FROM co_authors
     START WITH Autor2 = 'Pilipczuk Mi'
-    CONNECT BY NOCYCLE PRIOR Autor1 = Autor2 OR PRIOR Autor2 = Autor1
+    CONNECT BY NOCYCLE PRIOR Autor2 = Autor1 and LEVEL < 7
 ),
 result_with_levels AS (
     SELECT Autor, MIN(Level2) AS MinLevel
@@ -19,7 +19,7 @@ SELECT
     a.ID AS Autor,
     r.MinLevel AS Poziom
 FROM
-    Autorzy a
+    Autorzy2 a
 LEFT JOIN
     result_with_levels r ON a.ID = r.Autor
 ORDER BY
